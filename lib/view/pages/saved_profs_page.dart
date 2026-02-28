17f6cb0b4ee221030cb80e75f139f62cdcb0bb1a
pianex <pianex.bns@gmail.com>
2026-02-28 18:27:17 +0100

    lap

--- lib/view/pages/saved_profs_page.dart
+++ lib/view/pages/saved_profs_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:project_a/core/constants/app_theme.dart';
 import 'package:project_a/core/functions/prof_functions.dart';
